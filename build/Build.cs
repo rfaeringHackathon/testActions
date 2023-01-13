316a7b362843ebbba1157527d4ae63be88821cb0
Rasmus Larsen <rfl@cvation.com>
2023-01-13 11:24:22 +0100

    Added new project

--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Numerics;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
@@ -8,9 +9,11 @@ using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Utilities.Collections;
+using Serilog;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [GitHubActions(
     "continuous",
@@ -25,26 +28,40 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile, x => x.UnitTest);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Solution("./NukeApp/NukeApp.sln")]
+    readonly Solution Solution;
+
     Target Clean => _ => _
-        .Before(Restore)
+        .DependentFor(Restore)
         .Executes(() =>
         {
+            DotNet($"clean {Solution.Path}");
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNet($"restore {Solution.Path}");
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNet($"build {Solution.Path}");
+        });
+
+    Target UnitTest => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNet($"test {Solution.Path}");
         });
 
 }
+
